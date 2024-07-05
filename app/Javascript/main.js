1e42d016e29d51508eefb0aa4899d78f3c33f85e
techtobit <nurthern2016@gmail.com>
2024-07-06 02:28:16 +0600

    scrall color changeing mehtod added

--- app/Javascript/main.js
+++ app/Javascript/main.js
@@ -10,3 +10,40 @@ openMenuBtn.addEventListener('click', () => {
 close_MenuBtn.addEventListener('click', () => {
   menu.classList.remove('visible');
 });
+
+
+
+document.addEventListener('DOMContentLoaded', () => {
+  const navbar = document.getElementById('navbar');
+  const sections = document.querySelectorAll('.section');
+
+  const options = {
+    threshold: 0.5 
+  };
+
+  const observer = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        const sectionId = entry.target.id;
+        switch (sectionId) {
+          case 'wppool_index':
+            navbar.style.backgroundColor = '#F3F3F3';
+            break;
+          case 'companies':
+            navbar.style.backgroundColor = '#F3F3F3';
+            break;
+          case 'section3':
+            navbar.style.backgroundColor = '#D3D3D3';
+            break;
+          default:
+            navbar.style.backgroundColor = 'transparent';
+        }
+      }
+    });
+  }, options);
+
+  sections.forEach(section => {
+    observer.observe(section);
+  });
+	
+});
