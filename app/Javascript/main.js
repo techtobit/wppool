a606829f9a1523c0e168d2f3bd165b48ec8092e7
techtobit <nurthern2016@gmail.com>
2024-07-06 22:00:43 +0600

    section -2 : Section Header setup

--- app/Javascript/main.js
+++ app/Javascript/main.js
@@ -15,14 +15,16 @@ close_MenuBtn.addEventListener('click', () => {
 
 document.addEventListener('DOMContentLoaded', () => {
   const navbar = document.getElementById('navbar');
-	const navbarBtn = document.getElementById('download_btn')
+	const downloadBtn = document.getElementById('download_btn')
 	const shareBtn = document.getElementById('share_btn')
 	const logoWhite = document.getElementById('logo_white')
 	const logoBlack = document.getElementById('logo_black')
   const sections = document.querySelectorAll('.section');
 
+
+
   const options = {
-    threshold: 0.5 
+    threshold: 0.8 
   };
 
   const observer = new IntersectionObserver((entries) => {
@@ -31,9 +33,13 @@ document.addEventListener('DOMContentLoaded', () => {
         const sectionId = entry.target.id;
         switch (sectionId) {
           case 'wppool_index':
-            navbar.style.backgroundColor = '#F3F3F3';
-						navbarBtn.style.borderColor = '#2042B6';
-						shareBtn.style.borderColor = '#AFCD80';
+            navbar.style.backgroundColor = '#EFF3F6';
+						downloadBtn.style.borderColor = '#2042B6';
+						downloadBtn.style.color = '#000000';
+            shareBtn.style.borderColor = '#AFCD80';
+            logoWhite.style.display = 'none';
+						logoBlack.style.display = 'block';
+
             break;
           case 'companies':
             navbar.style.backgroundColor = '#F3F3F3';
@@ -45,8 +51,8 @@ document.addEventListener('DOMContentLoaded', () => {
             break;
           default:
             navbar.style.backgroundColor = 'transparent';
-						navbarBtn.style.borderColor = '#FFFFFF';
-						navbarBtn.style.color = '#FFFFFF';
+						downloadBtn.style.borderColor = '#FFFFFF';
+						downloadBtn.style.color = '#FFFFFF';
 						shareBtn.style.borderColor = '#FFFFFF';
 						logoBlack.style.display = 'none'
 						logoWhite.style.display = 'block'
